4d0af88cf16a9638415a708d6ab8f0f3a4b7d1fc
Toan Nguyen <tnguyen3537@gmail.com>
2024-09-17 01:42:05 -0500

    Adding Delete function and splitcomponent

--- src/component/todolist.jsx
+++ src/component/todolist.jsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import TodoTable from "./todotable";
+
 function Todolist() {
   const [todo, setTodo] = useState({ description: "", duedate: "" });
   const [todos, setTodos] = useState([]);
@@ -12,12 +14,16 @@ function Todolist() {
     }
   };
 
+  const handleDelete = (row) => {
+    setTodos(todos.filter((_, index) => row != index));
+  };
+
   return (
     <>
       <h3>My To Do List</h3>
       <label>Task</label>
       <input
-        placeholder="Type Task here"
+        placeholder="Type task here"
         value={todo.description}
         onChange={(event) =>
           setTodo({ ...todo, description: event.target.value })
@@ -25,22 +31,13 @@ function Todolist() {
       />
       <label>Due Date</label>
       <input
-        placeholder="Type Due Date"
+        type="date"
         value={todo.duedate}
         onChange={(event) => setTodo({ ...todo, duedate: event.target.value })}
       />
       <button onClick={handleTodo}>Add To Do</button>
 
-      <table>
-        <tbody>
-          {todos.map((todo, index) => (
-            <tr key={index}>
-              <td>{todo.description}</td>
-              <td>{todo.duedate}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <TodoTable todos={todos} handleDelete={handleDelete} />
     </>
   );
 }
---
+++ src/component/todotable.jsx
@@ -0,0 +1,23 @@
+function TodoTable(props) {
+  return (
+    <table>
+      <tbody>
+        <tr>
+          <th>Description</th>
+          <th>Date</th>
+          <th>index</th>
+        </tr>
+        {props.todos.map((todo, index) => (
+          <tr key={index}>
+            <td>{todo.description}</td>
+            <td>{todo.duedate}</td>
+            <td>{index}</td>
+            <td><button onClick={() =>props.handleDelete(index)}>Done</button></td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
+
+export default TodoTable;
